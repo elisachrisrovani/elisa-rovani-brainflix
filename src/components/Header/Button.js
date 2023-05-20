02c920b1ae5c9c6cd0c452f950b72f8ccc122c69
elisachrisrovani <elisachrisrovani@gmail.com>
2023-05-20 14:17:20 -0400

    desktop version for header component added

--- src/components/Header/Button.js
+++ src/components/Header/Button.js
@@ -1,11 +1,16 @@
 import Upload from '../../assets/images/upload.svg';
+import avatar from '../../assets/images/Mohan-muruge.jpg';
+
 import './Button.scss';
 
 export function Button(){
     return(
         <div class='button-container'>
+            <div className='button-container__area'>
             <img src={Upload} alt='upload'/>
             <h2>UPLOAD</h2>
+            </div>
+       
         </div>
     )
 }
