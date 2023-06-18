61d8c15e1b028f03c00b85d6c98c620d2695e897
elisachrisrovani <elisachrisrovani@gmail.com>
2023-06-18 11:02:46 -0400

    unused componentes excluded

--- src/App.js
+++ src/App.js
@@ -1,14 +1,7 @@
-import { useState } from 'react';
 import './App.scss';
-import { Header } from './components/Header/HeaderApp';
-import videosData from './data/video-details.json';
-import { HeroApp } from './components/Main/Hero/HeroApp';
-import { ListApp } from './components/Main/List/ListApp';
 import { BrowserRouter, Routes, Route, Link } from "react-router-dom";
-import { HeroVideo } from './components/Main/Hero/HeroVideo';
 import {Page} from './pages/page/page';
-import { Button } from './components/Header/Button';
-import { HomePage, homePage } from './pages/page/home';
+import { HomePage} from './pages/page/home';
 
 function App() {
 
--- src/pages/page/home.js
+++ src/pages/page/home.js
@@ -1,22 +1,9 @@
-
 import './home.scss';
 import { Header } from './../../components/Header/HeaderApp'
 import { HeroApp } from './../../components/Main/Hero/HeroApp';
-import { ListApp } from './../../components/Main/List/ListApp';
-import { HeroVideo } from './../../components/Main/Hero/HeroVideo';
-import { useState } from 'react';
-import videosData from './../../data/video-details.json';
 export function HomePage() {
 
-    // const [videos] = useState(videosData);
-    // const [activeVideo, setActiveVideo] = useState(videosData[0]);
-    // const [messages] = useState(videosData);
-
-    // const handleChangeActiveVideo = (id) =>{
-    //     const foundVideo = videos.find((video) => video.id === id);
-    //     setActiveVideo(foundVideo);
-    //   };
-
+   
   return (
      <div className="App-container">
 
